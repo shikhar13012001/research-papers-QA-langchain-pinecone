8e17e10ee17fcf8220fccd0975264e39c3b70451
shikhar13012001 <ishgupta2015@gmail.com>
2024-06-16 20:19:00 +0530

    fixed timing speed

--- app/page.tsx
+++ app/page.tsx
@@ -4,7 +4,8 @@ import {
 } from 'react'
 import { Button } from '@/components/ui/button'
 import { PieChartIcon } from '@radix-ui/react-icons'
-
+// These styles apply to every route in the application
+import './globals.css'
 export default function Home() {
   const [query, setQuery] = useState('')
   const [result, setResult] = useState('')
@@ -38,33 +39,36 @@ export default function Home() {
     }
   }
   return (
-    <main className="flex flex-1 flex-col justify-center items-center p-24">
-      <p
-        className='text-xl font-medium'
-      >
-      Langchain, Pinecone, and GPT with Next.js
+    <main className="flex flex-col justify-center items-center p-24 space-y-6 w-full">
+    <p className="text-4xl font-bold">
+      Langchain, Pinecone, and LLM with Next.js
+    </p>
+    <input
+      className="mt-3 rounded border w-[400px] text-black px-4 py-2"
+      onChange={e => setQuery(e.target.value)}
+      placeholder="Type your query here"
+    />
+    <Button
+    variant={"default"}
+      className="w-[400px] mt-3 bg-blue-500 text-white py-2 rounded hover:bg-blue-600"
+      onClick={sendQuery}
+    >
+      Ask AI
+    </Button>
+    {loading && <PieChartIcon className="my-5 w-8 h-8 animate-spin" />}
+    {result && (
+      <p className="my-8 border p-4 rounded w-[400px] bg-gray-900">
+        {result}
       </p>
-      <input
-      className='
-      mt-3
-      rounded border w-[400px]
-      text-black px-2 py-1' onChange={e => setQuery(e.target.value)} />
-      <Button
-      className='w-[400px] mt-3'
-      onClick={sendQuery}>Ask AI</Button>
-      {
-        loading && <PieChartIcon className='my-5 w-8 h-8 animate-spin' />
-      }
-      {
-        result && (
-          <p className='my-8 border p-4 rounded'>{result}</p>
-        )
-      }
-      { /* consider removing this button from the UI once the embeddings are created ... */}
-      <Button
-      className='w-[400px] mt-2'
-      variant='outline'
-      onClick={createIndexAndEmbeddings}>Create index and embeddings</Button>
-    </main>
+    )}
+    <Button
+      className="w-[400px] mt-2 border border-blue-500 text-blue-500 py-2 rounded hover:bg-blue-50"
+      variant="outline"
+      onClick={createIndexAndEmbeddings}
+    >
+      Create index and embeddings
+    </Button>
+  </main>
+  
   )
 }
--- utils.ts
+++ utils.ts
@@ -1,8 +1,11 @@
 import { FireworksEmbeddings } from "@langchain/community/embeddings/fireworks";
 import { RecursiveCharacterTextSplitter } from "langchain/text_splitter";
-import { Cohere } from "langchain/llms/cohere";
+import { createStuffDocumentsChain } from "langchain/chains/combine_documents";
+import { ChatPromptTemplate } from "@langchain/core/prompts";
 import { loadQAStuffChain } from "langchain/chains";
 import { Document } from "langchain/document";
+import fs from "fs";
+import { ChatOpenAI } from "@langchain/openai";
 import { indexName, timeout } from "./config";
 import { type Pinecone, type IndexModel } from "@pinecone-database/pinecone";
 
@@ -30,7 +33,7 @@ export const createPineconeIndex = async (
   const existingIndexes = await client.listIndexes();
   if (checkIfIndexExists(existingIndexes.indexes, indexName)) {
     console.log("Index already exists");
-    return;
+    await client.deleteIndex(indexName);
   }
   console.log("Creating Index: ", indexName);
   const pineconeIndexPromise = client.createIndex({
@@ -63,8 +66,12 @@ export const updatePineconeIndex = async (
   // retrieve pinecone index
   const index = client.index(indexName);
   console.log("Updating Pinecone Index: ", indexName);
-
+  // save to file docs.json and log the number of documents
+  console.log(`Saving ${docs.length} documents to docs.json`);
+  // save to file docs.json
+  fs.writeFileSync("docs.json", JSON.stringify(docs, null, 2));
   // insert data into pinecone index
+  let chunksArr: any = [];
   for (const doc of docs) {
     console.log("Inserting document: ", doc.metadata.source);
     const txtPath = doc.metadata.source;
@@ -78,38 +85,69 @@ export const updatePineconeIndex = async (
     console.log(
       `Calling OpenAI's Embedding endpoint documents with ${chunks.length} text chunks ...`
     );
-    // create openai embeddings
-    const embeddingsArrays = await getEmbeddings(chunks);
-
-    console.log("Finished embedding documents");
-    console.log(
-      `Creating ${chunks.length} vectors array with id, values, and metadata...`
-    );
-
-    const batchSize = 100;
-    let batch: any = [];
-    for (let i = 0; i < chunks.length; i++) {
-      const chunk = chunks[i];
-      const vector = {
-        id: `${txtPath}-${i}`,
-        values: embeddingsArrays[i],
-        metadata: {
-          ...chunk.metadata,
-          loc: JSON.stringify(chunk.metadata.loc),
-          pageContent: chunk.pageContent,
-          txtPath: txtPath,
-        },
+    const chunksObj = chunks.map((chunk: any) => {
+      return {
+        ...chunk,
+        textPath: txtPath,
       };
-      batch.push(vector);
-      if (batch.length === batchSize || i === chunks.length - 1) {
-        console.log(`Inserting ${batch.length} vectors into pinecone index`);
-        // upsert
-        await index.upsert(batch);
-        // empty batch
-        batch = [];
-      }
+    });
+    // create openai embeddings
+    chunksArr.push(chunksObj);
+    // flatten the chunks
+    chunksArr = chunksArr.flat();
+  }
+  console.log(
+    "Finished chunking all documents",
+    chunksArr.length,
+    chunksArr[0]
+  );
+
+  // get embeddings array in 100 100 batches
+  let embeddingsArrays: any = [];
+  const promises: Promise<any>[] = [];
+  for (let i = 0; i < chunksArr.length; i += 200) {
+    const promise = getEmbeddings(chunksArr.slice(i, i + 200));
+    promises.push(promise);
+  }
+
+  const results = await Promise.all(promises);
+  embeddingsArrays = results.flat();
+  // save to file embeddings.json
+  fs.writeFileSync(
+    "embeddings.json",
+    JSON.stringify(embeddingsArrays, null, 2)
+  );
+
+  const batchSize = 250;
+
+  let batch: any = [];
+  let pineconePromises: Promise<any>[] = [];
+  for (let i = 0; i < chunksArr.length; i++) {
+    const chunk = chunksArr[i];
+    const txtPath = chunk.textPath;
+    delete chunk.textPath;
+    const vector = {
+      id: `${txtPath}-${i}`,
+      values: embeddingsArrays[i],
+      metadata: {
+        ...chunk.metadata,
+        loc: JSON.stringify(chunk.metadata.loc),
+        pageContent: chunk.pageContent,
+        txtPath: txtPath,
+      },
+    };
+    batch.push(vector);
+    if (batch.length === batchSize || i === chunksArr.length - 1) {
+      console.log(`Inserting ${batch.length} vectors into pinecone index`);
+      // upsert
+      pineconePromises.push(index.upsert(batch));
+
+      // empty batch
+      batch = [];
     }
   }
+  // wait for all promises to resolve
+  await Promise.all(pineconePromises);
 };
 
 // query pinecone index
@@ -142,23 +180,41 @@ export const queryPineconeVectorStoreAndQueryLLM = async (
 
   // if we found anything
   if (queryResponse.matches.length) {
-    console.log("Found matches, querying COHERE...", queryResponse.matches);
-    const baseFireworkLLM = new Cohere({
-      apiKey: process.env.COHERE_API_KEY || "",
+    console.log(
+      "Found matches, querying COHERE...",
+      queryResponse.matches.length
+    );
+    // save the matches to a file
+    fs.writeFileSync(
+      "matches.json",
+      JSON.stringify(queryResponse.matches, null, 2)
+    );
+    const model = new ChatOpenAI({
+      modelName: "gpt-4o",
+      temperature: 0.2,
     });
-    const chain = await loadQAStuffChain(baseFireworkLLM, { verbose: true });
+    const prompt = ChatPromptTemplate.fromTemplate(
+      `Use the following pieces of context to answer the question in plain text at the end in technical details, keep the answer crisp and to the point. If you don't know the answer, just say that you don't know, don't try to make up an answer: 
+      {context}
+      Question: {question}`
+    );
+
+    const chain = await createStuffDocumentsChain({
+      llm: model,
+      prompt,
+    });
     const concatenatedPageContent = queryResponse.matches
       .map((match: any) => match.metadata.pageContent)
       .join(" ");
     const doc = new Document({ pageContent: concatenatedPageContent });
-
-    const result = await chain.call({
-      input_documents: [doc],
+    console.log(doc);
+    const result = await chain.invoke({
+      context: [doc],
       question: question,
     });
     // save with fs
-   
-    return result.text
+
+    return result;
   } else {
     // 11. Log that there are no matches, so GPT-3 will not be queried
     console.log("Since there are no matches, COHERE will not be queried.");
@@ -169,7 +225,7 @@ export const queryPineconeVectorStoreAndQueryLLM = async (
 
 async function getEmbeddings(chunks: any) {
   const fireworksEmbeddings = new FireworksEmbeddings({
-    apiKey: process.env.OPENAI_API_KEY || "",
+    apiKey: process.env.FIREWORKSAI_API_KEY || "",
   });
   const documentRes = await fireworksEmbeddings.embedDocuments(
     chunks.map((chunk: any) => chunk.pageContent.replace(/\n/g, ""))
